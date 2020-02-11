0c8ab1712b0e1d7f619029874f79b2df2a8d8732
Raj Kadam <raj.kadam@stylabs.in>
2020-02-11 11:22:49 +0530

    Initia project setup

---
+++ NumberToWord/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumberToWord
+//
+//  Created by Raj Kadam on 11/02/20.
+//  Copyright © 2020 2020_iOS_7456dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NumberToWord/ViewModel/NumberViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NumberViewModel.swift
+//  NumberToWord
+//
+//  Created by Raj Kadam on 11/02/20.
+//  Copyright © 2020 2020_iOS_7456dev. All rights reserved.
+//
+
+import Foundation
+
+class NumberViewModel: NSObject {
+    
+}
